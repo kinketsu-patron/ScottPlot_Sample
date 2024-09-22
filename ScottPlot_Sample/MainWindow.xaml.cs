f6b73958af621702ba5a849e21dcd6bb454ba2f8
yoshiosan <i.am.a.passing.kamen.rider@gmail.com>
2024-09-22 14:20:24 +0900

    新規登録

---
+++ ScottPlot_Sample/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+
+namespace ScottPlot_Sample
+{
+        /// <summary>
+        /// Interaction logic for MainWindow.xaml
+        /// </summary>
+        public partial class MainWindow : Window
+        {
+                public MainWindow( )
+                {
+                        InitializeComponent( );
+                        Loaded += LoadEvent;
+                }
+
+                private void LoadEvent( object sender, RoutedEventArgs e )
+                {
+                        int[] dataX = { 1, 2, 3, 4, 5 };
+                        int[] dataY = { 1, 4, 9, 16, 25 };
+                        WPF_Graph.Plot.Add.Scatter( dataX, dataY );
+                        WPF_Graph.Plot.Axes.SetLimits( 0, 10, 0, 30 );
+                        WPF_Graph.Refresh( );
+                }
+        }
+}
