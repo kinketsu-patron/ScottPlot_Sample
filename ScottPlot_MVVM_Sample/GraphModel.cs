77d2c29ad42f842803a09f3cf7c191a75ea296ed
yoshiosan <i.am.a.passing.kamen.rider@gmail.com>
2024-09-23 21:27:21 +0900

    MVVM仕様追加

---
+++ ScottPlot_MVVM_Sample/GraphModel.cs
@@ -0,0 +1,37 @@
+﻿using ScottPlot.WPF;
+
+namespace ScottPlot_MVVM_Sample
+{
+        public class GraphModel : NotifyChanged
+        {
+                #region メンバ変数
+                private WpfPlot? m_ScottPlotGraph = null;
+                #endregion
+
+                #region プロパティ
+                public WpfPlot? ScottPlotGraph
+                {
+                        get { return m_ScottPlotGraph; }
+                        set
+                        {
+                                if ( m_ScottPlotGraph != value ) {
+                                        m_ScottPlotGraph = value;
+                                        RaisePropertyChanged( );
+                                }
+                        }
+                }
+                #endregion
+
+                #region コンストラクタ
+                public GraphModel( )
+                {
+                        ScottPlotGraph = new WpfPlot( );
+                        int[] dataX = { 1, 2, 3, 4, 5 };
+                        int[] dataY = { 1, 4, 9, 16, 25 };
+                        ScottPlotGraph.Plot.Add.Scatter( dataX, dataY );
+                        ScottPlotGraph.Plot.Axes.SetLimits( 0, 10, 0, 30 );
+                        ScottPlotGraph.Refresh( );
+                }
+                #endregion
+        }
+}
---
+++ ScottPlot_MVVM_Sample/MainViewModel.cs
@@ -0,0 +1,27 @@
+﻿using ScottPlot.WPF;
+
+namespace ScottPlot_MVVM_Sample
+{
+        public class MainViewModel : NotifyChanged
+        {
+                #region メンバ変数
+                private GraphModel m_GraphModel;
+                #endregion
+
+                #region プロパティ
+                public WpfPlot? ScottPlotGraph
+                {
+                        get { return m_GraphModel.ScottPlotGraph; }
+                        set { m_GraphModel.ScottPlotGraph = value; }
+                }
+                #endregion
+
+                #region コンストラクタ
+                public MainViewModel( )
+                {
+                        m_GraphModel = new GraphModel( );
+                        m_GraphModel.PropertyChanged += ( sender, e ) => RaisePropertyChanged( );
+                }
+                #endregion
+        }
+}
